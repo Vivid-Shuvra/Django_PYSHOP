2a54d374590d88d409ca0c9f9b4388fe8d34a34a
Vivid-Shuvra <53150915+Vivid-Shuvra@users.noreply.github.com>
2020-10-12 12:15:00 +0600

    ..

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-
-]
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT})
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
